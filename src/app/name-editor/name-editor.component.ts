2c9707397e1bb5e0a5aadaef0a644870d97f0379
Kate Sky <kateskytech@outlook.com>
2020-05-08 11:40:27 -0500

    formatting

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,26 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import {debounceTime} from 'rxjs/operators';
-import {  Store } from '@ngrx/store';
-import {  RootState, LogAction } from '../reducers';
+import { debounceTime } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { RootState, LogAction } from '../reducers';
 @Component({
   selector: 'app-name-editor',
   templateUrl: './name-editor.component.html',
-  styleUrls: ['./name-editor.component.scss']
+  styleUrls: ['./name-editor.component.scss'],
 })
 export class NameEditorComponent implements OnInit {
-  name = new FormControl('',Validators.required );
+  name = new FormControl('', Validators.required);
 
-  constructor(private store: Store<RootState>) {
-    this.name.valueChanges.pipe(debounceTime(300)).subscribe(value=>{
-      debugger;
-      if(value.length> 0){
-        this.store.dispatch(LogAction({user:value}));
-      }
-    })
-   }
+  constructor(private store: Store<RootState>) {}
 
   ngOnInit(): void {
+    this.name.valueChanges.pipe(debounceTime(300)).subscribe((value) => {
+      this.store.dispatch(LogAction({ user: value }));
+    });
   }
   updateName() {
     this.name.setValue('Nancy');
